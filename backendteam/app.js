90d14f01f0e03b424e518fa1f796bc4189bdb32b
anggita ruzty <ruzty.anggita@gmail.com>
2021-08-05 14:35:30 +0700

    init project

---
+++ backendteam/app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
+
+const server = express();
+const port = process.env.PORT || 3000;
+
+server.use(logger("dev"));
+server.use(cors());
+server.use(express.json());
+server.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+server.get("/", (req, res) => {
+  res.send("Audiophile");
+});
+
+server.all("*", (req, res) => {
+  res.status(404).json({
+    statusText: "Not Found",
+    message: "You Have Trying Reaching A Route That Doesn't Exist",
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
